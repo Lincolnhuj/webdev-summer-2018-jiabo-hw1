5880409ecc488de6d6f6a3e788dd57c36f715fcf
Jiabo Hu <lincolnhuj@gmail.com>
2018-05-13 18:07:30 -0700

    hw1: added hello model, service, and repository. Now we can connect spring to mysql.

---
+++ src/main/java/com/example/webdevsummer2018/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.webdevsummer2018;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //This annotation mark the class as entry to spring boot.
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
